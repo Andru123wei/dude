686b4c169d8b16761615aeec396f9aa7641aeabc
andresmro <andru2001@hotmail.es>
2022-06-01 17:05:27 -0500

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+import dudeN from './images/dudeN.png';
 import './App.css';
 
 function App() {
   return (
     <>
-    <div className="App">
+    <div className=" flex text-9xl">
       DUDE
+      
     </div>
+    <img className=' w-100px h-96' src={dudeN}/>
     
     </>
   );
