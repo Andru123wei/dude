c5d649015bd2ca36bcf9a453b3837d8018f85455
Jarol <lozanojarol0202@gmail.com>
2022-06-01 20:46:41 -0500

    formualrio login

--- src/App.js
+++ src/App.js
@@ -5,19 +5,32 @@ import './App.css';
 function App() {
   return (
     <>
+  <div className='ultra-div'>
+    
+      <div className='formu'>
+        <label>Username</label>
+        <form>
+          <input type="text" placeholder='Username' />
+        </form>
+        <label>Password</label>
+        <form>
+          <input type="password" placeholder='Password' />
+        </form>
+      </div>  
+
     <div className="mega-div">
 
     <div>
-    <button><img className='icon'  src={icD}/></button>
+      <button><img className='icon'  src={icD}/></button>
     </div>
 
     <div>
-    <img className='dude' src={dudeN}/>
+      <img className='dude' src={dudeN}/>
     </div>
 
     </div>
     
-    
+    </div>
     </>
   );
 }
