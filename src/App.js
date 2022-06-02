67b9c8f0bb036784ce66d6659a7bd244494d9fc6
andresmro <andru2001@hotmail.es>
2022-06-02 15:49:30 -0500

    cambio en signup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import dudeN from './images/dudeN.png';
-import icD from './images/icon.png';
+
+import Login from './components/Login';
 import './App.css';
+import { Main } from './components/Main';
 
 function App() {
   return (
-    <>
-  <div className='ultra-div'>
-    
-      <div className='formu'>
-        <label>Username</label>
-        <form>
-          <input type="text" placeholder='Username' />
-        </form>
-        <label>Password</label>
-        <form>
-          <input type="password" placeholder='Password' />
-        </form>
-      </div>  
-
-    <div className="mega-div">
-
-    <div>
-      <button><img className='icon'  src={icD}/></button>
-    </div>
-
-    <div>
-      <img className='dude' src={dudeN}/>
-    </div>
-
-    </div>
-    
-    </div>
+    <>   
+     <Login/>
     </>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import dudeN from '../images/dudeN.png';
+import icD from '../images/icon.png';
+import '../styles/login.css';
+
+function Login() {
+
+    const [value, setValue] = useState({
+        email: '',
+        username: '',
+        password: '',
+    });
+
+    const handleInputChange = (e) => {
+        setValue({
+            ...value,
+            [e.target.name] : e.target.value
+        });
+        console.log(value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`enviando datos... ${value.email}, ${value.username}, ${value.password}`)
+    }
+
+  return (
+    <>
+    <div className='ultra-div'>
+
+        <div className="signupFrm">
+            <form className="form" onSubmit={handleSubmit}>
+                <h1 className="title">Sign up</h1>
+
+                <div className="inputContainer">
+                    <input type='email' className='input' placeholder="a" onChange={handleInputChange} name='email'/>
+                    <label className="label">Email</label>
+                </div>
+
+                <div className="inputContainer">
+                    <input type='text' className="input" placeholder="a" onChange={handleInputChange} name='username'/>
+                    <label className="label">Username</label>
+                </div>
+
+                <div className="inputContainer">
+                    <input type='password' className="input" placeholder="a" onChange={handleInputChange} name='password'/>
+                    <label className="label">Password</label>
+                 </div>
+
+                <div className="inputContainer">
+                    <input type="password" className="input" placeholder="a"/>
+                    <label className="label">Confirm Password</label>
+                </div>
+
+                <input type='submit' className="submitBtn" value="Sign up"/>
+            </form>
+        </div>
+
+        <div className="mega-div">
+            <button><img className='icon' src={icD} /></button>
+            <img className='dude' src={dudeN} />
+        </div>
+
+    </div>
+    </>
+  );
+}
+
+export default Login;
