1befd22bed442880d118e9dcbef7aa769ef953cd
Jarol <lozanojarol0202@gmail.com>
2022-06-07 12:54:49 -0500

    arreglo

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,23 +2,24 @@ import { useState } from 'react';
 import dudeN from '../images/dudeN.png';
 import icD from '../images/icon.png';
 import '../styles/login.css';
-import sonidoencima from '../assets/sounds/triston.mp3'
+import sonidoencima from'../assets/sounds/triston.mp3'
 import { motion } from 'framer-motion'
 import ReactHowler from 'react-howler'
 
 function Login() {
 
     const [value, setValue] = useState({
+        email: '',
         username: '',
         password: '',
     });
 
-    const [dudu, setDudu] = useState(false);
+    const [dudu,setDudu] = useState(false);
 
     const handleInputChange = (e) => {
         setValue({
             ...value,
-            [e.target.name]: e.target.value
+            [e.target.name] : e.target.value
         });
         console.log(value);
     }
@@ -28,54 +29,59 @@ function Login() {
         console.log(`enviando datos... ${value.username}, ${value.password}`)
     }
 
-    return (
-        <>
-            <div className="signupFrm">
-                <form className="form" onSubmit={handleSubmit}>
-                    <h1 className="title">Sign In</h1>
+  return (
+    <>
+    <div className='ultra-div'>
 
-                    <div className="inputContainer">
-                        <input type='text' className="input" placeholder="a" onChange={handleInputChange} name='username' />
-                        <label className="label">Username</label>
-                    </div>
+        <div className="signupFrm">
+            <form className="form" onSubmit={handleSubmit}>
+                <h1 className="title">Sign up</h1>
 
-                    <div className="inputContainer">
-                        <input type='password' className="input" placeholder="a" onChange={handleInputChange} name='password' />
-                        <label className="label">Password</label>
-                    </div>
-
-                    <input type='submit' className="submitBtn" value="Sign In" />
-                </form>
-
-                <div className='formR'>
-                    No account?, <a href='#'>Sign Up here</a>
+                <div className="inputContainer">
+                    <input type='text' className="input" placeholder="a" onChange={handleInputChange} name='username'/>
+                    <label className="label">Username</label>
                 </div>
+
+                <div className="inputContainer">
+                    <input type='password' className="input" placeholder="a" onChange={handleInputChange} name='password'/>
+                    <label className="label">Password</label>
+                 </div>
+
+                <input type='submit' className="submitBtn" value="Sign In"/>
+            </form>
+
+            <div className='formR'>
+                No account?, <a href='#'>Sign Up here</a>
             </div>
-            <div className="mega-div">
+        </div>
 
-                <button onClick={() => setDudu(true)}><motion.img
-                    animate={{ rotate: 40 }}
-                    transition={{
-                        duration: 0.4,
-                        repeat: 10,
-                        repeatType: "reverse"
-                    }}
-
-                    className='icon' src={icD} />
-                    <ReactHowler
-                        src={sonidoencima}
-                        playing={dudu}
-                        volume={0.3}
-                        onEnd={() => setDudu(false)} />
-
-                </button>
-                <img className='dude' alt='dude' src={dudeN} />
-            </div>
-        </>
-    );
+        <div className="mega-div">
+       
+            <button onClick={()=>setDudu(true)}><motion.img 
+                animate={{ rotate: 40 }}
+                transition={{ 
+                    duration: 0.4,
+                    repeat : 10,
+                    repeatType: "reverse" 
+                }}
+                
+                className='icon'  src={icD} />
+                 <ReactHowler
+                    src={sonidoencima}
+                    playing={dudu}
+                    volume={0.3}
+                    onEnd={()=>setDudu(false)}
+                 />
+               
+            </button>
+            <img className='dude' alt='dude' src={dudeN} />
+        </div>
+    </div>
+    </>
+  );
 }
 
 
-
+  
 
 export default Login;
