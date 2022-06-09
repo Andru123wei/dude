5df498ee17426697bbfdec14675880f74a8d7d7c
Jarol <lozanojarol0202@gmail.com>
2022-06-09 18:24:43 -0500

    notis

--- src/components/homeComponents/Noticias.js
+++ src/components/homeComponents/Noticias.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from 'react';
+import noti from '../../data/arraynoti';
 import '../stylesHome/noticias.css';
 
 const Noticias = () => {
   return (
     <div className='noticias'>
-          <label>noticias</label>
+
+      <ul className='lista-noti'>
+        {noti.map((item, idx)=>(
+
+          <li key={idx} className='lista-lista'> 
+          <div className='cada-noti'>
+            <div className='owner'>
+            {item.owner}
+
+            </div>
+            <div className='desc'>
+              {item.descrip}
+
+            </div>
+            <img src={item.img} alt={idx} />
+          </div>
+          </li>
+
+
+        ))}
+      </ul>
+         
         </div>
   )
 }
---
+++ src/data/arraynoti.js
@@ -0,0 +1,25 @@
+const noti = [
+    {
+        owner: "Noticiasmuyfalsas",
+        descrip: "La nasa descubre que la tierra es plana, no podras creerlo!!!!",
+        img: 'https://cdn.discordapp.com/attachments/726296736888520765/984577286294618173/1.png',
+    },
+    {
+        owner: "Noticias secas",
+        descrip: "“Demanda a su esposa por tener hijos feos”",
+        img: 'https://cdn.discordapp.com/attachments/726296736888520765/984577286646951987/2.png',
+    },
+    {
+        owner: "PetroNews",
+        descrip: "Revelan polémico video de Petro comiendo pizza hawaiana.",
+        img: 'https://cdn.discordapp.com/attachments/726296736888520765/984577286919577691/3.png',
+    },
+    {
+        owner: "No creeras la ultima",
+        descrip: "13 cosas menos frágiles que Neymar según la ciencia",
+        img: 'https://cdn.discordapp.com/attachments/726296736888520765/984577287183798272/4.png',
+    },
+
+]
+
+export default noti;
